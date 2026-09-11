fbf713cbd6febd1f339ea685a0f9cead8df2c61b
Terje Moe <terjemoe54@hotmail.com>
2026-09-11 15:48:26 +0200

    Initial Commit

---
+++ SpendlyAI/SpendlyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendlyAIApp.swift
+//  SpendlyAI
+//
+//  Created by Terje Moe on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpendlyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
